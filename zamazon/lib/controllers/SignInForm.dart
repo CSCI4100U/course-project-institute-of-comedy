6db735d9a39377f5d11a9e200514acb68c46a68d
Ravi-Pogaku <ravichandra.pogaku@ontariotechu.net>
2022-11-09 23:09:40 -0500

    commit before merge

--- zamazon/lib/controllers/SignInForm.dart
+++ zamazon/lib/controllers/SignInForm.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class SignInWidget extends StatefulWidget{
+class SignInWidget extends StatefulWidget {
   SignInWidget({Key? key, this.title}) : super(key: key);
 
   final String? title;
@@ -8,45 +8,51 @@ class SignInWidget extends StatefulWidget{
   State<SignInWidget> createState() => _SignInWidgetState();
 }
 
-class _SignInWidgetState extends State<SignInWidget>{
+class _SignInWidgetState extends State<SignInWidget> {
   final _formKey = GlobalKey<FormState>();
 
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Sign In Page'),
       ),
       body: Column(
         children: [
-          Padding(padding: EdgeInsets.only(top:150, bottom: 10),
-            child: Text('Signing In',style: TextStyle(fontSize: 50),),
+          Padding(
+            padding: EdgeInsets.only(top: 150, bottom: 10),
+            child: Text(
+              'Signing In',
+              style: TextStyle(fontSize: 50),
+            ),
           ),
-          Padding(//Input form for Email
-              padding: EdgeInsets.only(top:0),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
+          Padding(
+            //Input form for Email
+            padding: EdgeInsets.only(top: 0),
+            child: TextField(
+              obscureText: true,
+              decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Email',
-                    hintText: 'Email example: abc@gmail.com'
-                ),
-              ),
+                  hintText: 'Email example: abc@gmail.com'),
+            ),
           ),
-          Padding(//Input form for Password
-            padding: EdgeInsets.only(bottom:0),
+          Padding(
+            //Input form for Password
+            padding: EdgeInsets.only(bottom: 0),
             child: TextField(
               obscureText: true,
               decoration: InputDecoration(
                   border: OutlineInputBorder(),
                   labelText: 'Password',
-                  hintText: 'Atleast 6 letters with 1 number.'
-              ),
+                  hintText: 'Atleast 6 letters with 1 number.'),
             ),
           ),
-          ElevatedButton(//Confirmed sign in and return to home page as logged in user
-              onPressed: (){Navigator.of(context).popUntil((route) => route.isFirst);},
-              child: Text('Continue')
-          )
+          ElevatedButton(
+              //Confirmed sign in and return to home page as logged in user
+              onPressed: () {
+                Navigator.of(context).popUntil((route) => route.isFirst);
+              },
+              child: Text('Continue'))
         ],
       ),
     );
